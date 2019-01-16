13fc3317bbf73c88f1a10edb8fb60df82d1247d1
Erich Purpur <36010345+epurpur@users.noreply.github.com>
2019-01-16 09:55:59 -0500

    Add files via upload

---
+++ IntroToPython.py
@@ -0,0 +1,86 @@
+
+#Strings
+variable1 = "apple"
+variable2 = "1234"
+variable3 = "abc123"
+variable4 = "this is also a string"
+
+
+
+
+#Numbers
+number1 = 4
+number2 = 3.14
+number3 = 4 + 1
+number4 = number1 + 1
+number5 = variable1 + variable2
+#number6 = number1 + variable1
+
+
+
+
+##Functions
+##built-ins like print(), type()
+#print(number1)
+#print("Number 1 =", number1)
+#print(type(number1))
+#print(type(number2))
+#
+##def function1():
+##    print("This is running from inside function1")
+##function1()
+
+#def adding_function(number):
+#    a = 2
+#    new_number = number + a
+#    print("New number =", new_number)
+#adding_function(number1)
+
+
+
+#Lists, loops, booleans, and indexing
+#numbers = [1, 2, 3, 4, 5]
+#cities = ['New York', 'London', 'Bangkok', 'Tokyo', 'Sydney']
+#print(cities)
+#cities.append('Charlottesville')
+#print(cities)
+
+#for number in numbers:
+#    print(number)
+##
+##for city in cities:
+##    print(city)
+#    
+#print(cities[1])
+#
+#x = 10
+#z = 1
+#while z < 10:
+#    print(z)
+#    z = z + 1  #or z+=1
+#    
+    
+    
+    
+    
+##Dictionaries
+#athletes = {'Michael Jordan': 'Basketball',
+#            'Michael Phelps': 'Swimming',
+#            'Jack Nicklaus': 'Golf',
+#            'Michaela Shiffrin': 'Skiing',
+#            'Drew Brees': 'Football',
+#            'Leo Messi': 'Soccer'}
+#
+#for keys, values in athletes.items():
+#    print(keys,"-", values) 
+#    
+#print(athletes['Michael Jordan'])
+##print(athletes[2])
+
+
+
+
+
+    
+    
+
