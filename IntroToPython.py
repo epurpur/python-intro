0b1467dd597bab420f19861cf5b174e6a8f51966
Erich Purpur <36010345+epurpur@users.noreply.github.com>
2019-01-18 15:55:13 -0500

    Add files via upload

--- IntroToPython.py
+++ IntroToPython.py
@@ -1,9 +1,23 @@
 
-##Strings
+
+
+
+#Strings & variables
 #variable1 = "apple"
 #variable2 = "1234"
 #variable3 = "abc123"
 #variable4 = "this is also a string"
+
+
+
+
+
+
+#Print Statement - A built-in function used to write output to console or elsewhere
+#print("hello world")
+#print(variable1)
+
+
 
 
 
@@ -19,8 +33,20 @@
 
 
 
+
+
+#Error Messages - Read them from the bottom up. Get used to reading error messages!
+#print('hello world)
+
+
+
+
+
+
 ##Functions
 ##built-ins like print(), type()
+
+#number1 = 4
 #print(number1)
 #print("Number 1 =", number1)
 #print(type(number1))
@@ -30,11 +56,15 @@
 ##    print("This is running from inside function1")
 ##function1()
 
-#def adding_function(number):
+#def adding_function(number):                               #passing in a variable as argument to the function
 #    a = 2
 #    new_number = number + a
 #    print("New number =", new_number)
 #adding_function(number1)
+
+
+
+
 
 
 
@@ -46,7 +76,7 @@
 #cities.append('Charlottesville')
 #print(cities)
 
-#for number in numbers:
+#for number in numbers:                         #for loop
 #    print(number)
 ##
 ##for city in cities:
@@ -54,26 +84,47 @@
 #    
 #print(cities[1])
 #
-#x = 10
 #z = 1
-#while z < 10:
+#while z < 10:                                  #while loop
 #    print(z)
-#    z = z + 1  #or z+=1
-#    
+#    z = z + 1
+#
+#b = 7
+#while b < 10:                                  #watch out for endless loop!
+#    print("B is less than 10")
+
+
     
     
+
+
     
     
 ##Dictionaries  (key, value pairs)
 #athletes = {'Michael Jordan': 'Basketball',
-#            'Michael Phelps': 'Swimming',
-#            'Leo Messi': 'Soccer'}
+#            'Wayne Gretzky': 'Hockey',
+#            'Leo Messi': 'Soccer',
+#            'Roger Federer': 'Tennis'}
 #
 #for keys, values in athletes.items():
-#    print(keys,"-", values) 
+#    print(keys,"plays", values)
+#
+#for values in athletes.values():
+#    print(values)
 #    
 #print(athletes['Michael Jordan'])
-##print(athletes[2])
+#print(athletes[2])                             #dicts do not support indexing!
+#
+#athletes['Peyton Manning'] = 'Football'
+#print(athletes)
+
+#MLB_Teams = {'Atlanta Braves': ['National League', 'Georgia', 'SunTrust Park'],
+#            'New York Yankees': ['American League', 'New York', 'Yankee Stadium'],
+#            'Chicago Cubs': ['National League', 'Chicago', 'Wrigley Field']}
+
+
+
+
 
 
 
@@ -82,6 +133,8 @@
 #This lets you bring in code from other places and is hugely powerful
 #import random
 #print(random.randint(0,20))
+
+
 
 
 
@@ -100,4 +153,19 @@
 
     
     
+
+
+
+#Mutable and Immutable data types
+#Some data types can be changed, others cant
+
+#tuple1 = (0, 1, 2, 3)
+#tuple1[0] = 5
+#print(tuple1)
+
+#list1 = ['blue', 'red', 'yellow', 'orange']
+#print(list1)
+#list1[0] = 'green'
+#print(list1)
+
 
